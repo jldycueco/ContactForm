1ccb968e644c6475a3b48baa3d35d7b85df74f01
JD <laeim017@gmail.com>
2019-11-14 11:06:30 +0800

    modified package.json scripts

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import express from 'express';
-
+import path from 'path';
 import cors from 'cors';
 
 import nodemailer from './routes/nodemailer';
@@ -13,6 +13,15 @@ app.use(express.urlencoded({ extended: false }));
 
 //Nodemailer Route
 app.use('/send', nodemailer);
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+/* The "catchall" handler: for any request that doesn't
+match one above, send back React's index.html file. */
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '../client/build/index.html'));
+});
 
 const port = 5000;
 app.listen(port, () =>
